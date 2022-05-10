9b05ceeeea83823169e258ce4b800d0200ddac29
HabacucMX <habacuc.vera@gmail.com>
2022-05-10 00:07:58 -0500

    Layout for hud

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,36 @@
 <template>
-    <div class="bg-gray-400 w-full h-full">
-        <h1 class="text-3xl">This is a default layout. Layouts can be found in the <span class="italic">layouts</span> dir</h1>
-
-        <div class=" p-28">
-            <div class="bg-white h-96">
-                <!-- The actual page will be inserted here -->
-                <Nuxt />
+    <div class="py-10 px-2">
+        <nav class="top-hud">
+            <div class="hud1con">Wallet</div>
+        
+          <div class="hud3">
+            <span> 🏢 </span>
+            <span className="mx-2">2</span>
+          </div>
+          <div class="hud3">
+            <span> ⚡ </span>
+            <span class="mx-2">20</span>
+          </div>
+          <div class="hud4">
+            <span> 💽 </span>
+            <span class="mx-2">12</span>
+          </div>
+        </nav>
+        <!-- The actual page will be inserted here -->
+        <Nuxt />
+        <nav class="bottom-hud">
+            <div class="huda">
+                New business NFT
             </div>
-        </div>
-    </div>
+            <div class="hudb">
+                My business
+            </div>
+            <div class="hudb">
+                My city business
+            </div>
+            <div class="hudd">
+                All business
+            </div>
+        </nav>
+   </div>
 </template>
