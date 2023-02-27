49f3dd640f1936f53b420e53b610a0ad6373c701
HabacucMX <habacuc.vera@gmail.com>
2023-02-26 20:55:37 -0600

    color update

--- pages/business-detail/index.vue
+++ pages/business-detail/index.vue
@@ -8,7 +8,7 @@
               class="relative bg-white/10 rounded-xl p-3 grid grid-cols-2 items-center text-center py-10 xl:py-3"
             >
               <div
-                class="bg-glass-200 absolute p-2 rounded-tl-xl rounded-br-xl top-0 left-0 text-2xl text-white/90"
+                class="bg-white/10 absolute p-2 rounded-tl-xl rounded-br-xl top-0 left-0 text-2xl text-white/90"
               >
                 🏪 <span class="text-sm">Stores</span>
               </div>
@@ -28,10 +28,10 @@
             </div>
 
             <div
-              class="relative bg-cyber-100/40 rounded-xl p-3 grid grid-cols-3 items-center text-center py-10 xl:py-3"
+              class="relative bg-white/10 rounded-xl p-3 grid grid-cols-3 items-center text-center py-10 xl:py-3"
             >
               <div
-                class="bg-glass-200 absolute p-2 rounded-tl-xl rounded-b-xl r-xl top-0 left-0 text-2xl text-white/90"
+                class="bg-deco-100/40 absolute p-2 rounded-tl-xl rounded-b-xl r-xl top-0 left-0 text-2xl text-white/90"
               >
                 🏦 <span class="text-sm">Vault</span>
               </div>
@@ -57,17 +57,17 @@
                     v-model="depositedAmount"
                     type="text"
                     name="price"
-                    class="w-full rounded-gen border-white/25 pl-5 focus:border-green-100 focus:ring-green-100 text-xl focus:text-green-100"
+                    class="w-full rounded-gen border-white/25 pl-5 focus:border-green-100 focus:ring-green-100 text-xl"
                     placeholder="0.00 DAI"
                   >
                   <div
-                    class="absolute top-1/2 -translate-y-1/2 right-0 cursor-pointer hover:text-cyber-100"
+                    class="absolute top-1/2 -translate-y-1/2 right-0 cursor-pointer hover:text-cyber-200"
                     @click="setMaxDAI()"
                   >
                     max
                   </div>
                   <div
-                    class="absolute -bottom-5 left-10 cursor-pointer hover:text-cyber-100 text-sm text-white/50"
+                    class="absolute -bottom-5 left-10  text-sm text-white/50"
                   >
                     <div>
                       {{ userDAI / Math.pow(10,18) }} <span>DAI</span>
