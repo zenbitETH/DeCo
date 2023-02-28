8e763d3c6bac5879ad3a25ff570b54938d0fa79d
HabacucMX <habacuc.vera@gmail.com>
2023-02-28 12:49:37 -0600

    update on Manage page

--- pages/business-detail/index.vue
+++ pages/business-detail/index.vue
@@ -3,12 +3,12 @@
     <section class="text-white">
       <div class="MainScreen">
         <div class="MainBoard">
-          <div class="xl:col-span-12 grid xl:grid-cols-2 gap-3">
+          <div class="xl:col-span-12 grid xl:grid-cols-3 gap-3">
             <div
               class="relative bg-white/10 rounded-xl p-3 grid grid-cols-2 items-center text-center py-10 xl:py-3"
             >
               <div
-                class="bg-white/10 absolute p-2 rounded-tl-xl rounded-br-xl top-0 left-0 text-2xl text-white/90"
+                class="bg-deco-200/40 absolute p-2 rounded-tl-xl rounded-br-xl top-0 left-0 text-2xl text-white/90"
               >
                 🏪 <span class="text-sm">Stores</span>
               </div>
@@ -23,31 +23,26 @@
                 <div class="text-2xl">
                   total sales
                 </div>
-                ${{ totalSales / 100 }} <span class="xl:text-xl text-sm">DAI</span>
+                ${{ totalSales / Math.pow(10,18) }} <span class="xl:text-xl text-sm">DAI</span>
               </div>
             </div>
 
             <div
-              class="relative bg-white/10 rounded-xl p-3 grid grid-cols-3 items-center text-center py-10 xl:py-3"
+              class="relative bg-white/10 rounded-xl p-3 grid grid-cols-2 items-center text-center py-10 xl:py-3"
             >
               <div
                 class="bg-deco-100/40 absolute p-2 rounded-tl-xl rounded-b-xl r-xl top-0 left-0 text-2xl text-white/90"
               >
                 🏦 <span class="text-sm">Vault</span>
               </div>
-              <div class="grid gap-5">
+              <div class="grid gap-5 col-span-2">
                 <div>
                   <div class="xl:text-base text-sm">
-                    APY <span class="text-4xl">1.3%</span>
-                  </div>
-                </div>
-                <div>
-                  <div class="xl:text-base text-sm">
-                    Balance
+                    Balance on vault
                   </div>
                   <span
                     class="text-4xl"
-                  >${{ totalBalance / Math.pow(10,18) }}<span class="xl:text-xl text-sm">DAI</span></span>
+                  >${{ userDAI / Math.pow(10,18) }} <span class="xl:text-xl text-sm">DAI</span></span>
                 </div>
               </div>
               <div class="grid grid-cols-2 col-span-2 items-center">
@@ -77,10 +72,66 @@
                 </div>
                 <div class="grid gap-5 px-5">
                   <div class="mintButton" @click="aaveDeposit()">
-                    supply
+                    supply to aave
                   </div>
                   <div class="mintButton" @click="aaveWithdraw()">
-                    withdraw
+                    withdraw to wallet
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              class="relative bg-white/10 rounded-xl p-3 grid grid-cols-2 grid-rows-2 items-center text-center py-10 xl:py-3"
+            >
+              <div
+                class="bg-gradient-to-b from-glass-700 to-glass-800 absolute p-2 rounded-tl-xl rounded-b-xl r-xl top-0 left-0 text-2xl text-white/90"
+              >
+                👻 <span class="text-sm">Aave</span>
+              </div>
+              <div class="grid gap-5 grid-cols-2 col-span-2 mx-12 items-center">
+                <div>
+                  <div class="xl:text-base text-sm">
+                    Balance on Aave
+                  </div>
+                  <span
+                    class="text-4xl"
+                  >$<span class="xl:text-xl text-sm">DAI</span></span>
+                </div>
+                <div>
+                  <div class="xl:text-base text-sm">
+                    <div>APY</div>
+                    <span class="text-4xl">1.3%</span>
+                  </div>
+                </div>
+              </div>
+              <div class="grid grid-cols-2 col-span-2 items-center">
+                <div class="relative pl-5">
+                  <input
+                    id="price"
+                    v-model="depositedAmount"
+                    type="text"
+                    name="price"
+                    class="w-full rounded-gen border-white/25 pl-5 focus:border-green-100 focus:ring-green-100 text-xl"
+                    placeholder="0.00 DAI"
+                  >
+                  <div
+                    class="absolute top-1/2 -translate-y-1/2 right-0 cursor-pointer hover:text-cyber-200"
+                    @click="setMaxDAI()"
+                  >
+                    max
+                  </div>
+                  <div
+                    class="absolute -bottom-5 left-10  text-sm text-white/50"
+                  >
+                    <div>
+                      <span>DAI</span>
+                      <span>available</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="grid gap-5 px-5">
+                  <div class="mintButton" @click="aaveWithdraw()">
+                    withdraw to vault
                   </div>
                 </div>
               </div>
