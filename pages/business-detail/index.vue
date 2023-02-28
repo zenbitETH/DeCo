05ab5333ef9b06eb4d640c514cf66e8a06cffef4
HabacucMX <habacuc.vera@gmail.com>
2023-02-28 13:25:18 -0600

    No voting on Manage page

--- pages/business-detail/index.vue
+++ pages/business-detail/index.vue
@@ -141,33 +141,17 @@
             <div class="grid md:grid-cols-4 xl:grid-cols-6 grid-cols-2 gap-5">
               <div v-for="business in businesses" :key="business.tokenId">
                 <nuxt-link :to="'business-detail/' + business.tokenId">
-                  <div class="DecoBG bg-glass-200">
+                  <div class="DecoBG">
                     <div class="text-lg text-center">
                       <img
                         class="fhd:h-full mx-auto"
                         :src="'/3dAssets/' + business.businessType + '.png'"
                       >
-                      <!-- <img class="xl:h-32 qhd:h-43 h-32 mx-auto" src="/deco logo.svg"> -->
                       {{ business.shortname }}
-                    </div>
-                    <div
-                      class="bg-glass-600 rounded-b-tf grid grid-cols-2 text-center text-base py-2"
-                    >
-                      <div>{{ business.upVotes }} 👍</div>
-                      <div>{{ business.downVotes }} 👎</div>
                     </div>
                   </div>
                 </nuxt-link>
               </div>
-            <!-- <nuxt-link to="business-detail-customer" class="DecoBG bg-glass-500">
-              <div class="">
-                <img class="xl:h-32 qhd:h-43 h-32 mx-auto" src="../../static/deco logo.svg">
-              </div>
-              <div class="bg-deco-900 rounded-b-tf grid grid-cols-2 text-center text-base py-2">
-                <div>0 👍</div>
-                <div>0 🛍️</div>
-              </div>
-            </nuxt-link> -->
             </div>
           </div>
         </div>
