16d9f0c8ed2ff07775a91e37dc8f904e9a1776a2
Krisztian Barta <bartariki@gmail.com>
2023-03-06 18:04:35 +0100

    Sold Services - Riki

---
+++ contracts/service-nft/listMySoldServices.js
@@ -0,0 +1,20 @@
+
+import Moralis from 'moralis'
+import { normalizeContractOutput } from '../methods'
+import abi from './abi.json'
+
+export default async function (address, businessId) {
+  const options = {
+    contractAddress: address,
+    functionName: 'getSoldServices',
+    abi,
+    params: {
+      _businessId: parseInt(businessId)
+    }
+  }
+  const result = await Moralis.executeFunction(options)
+  console.log(result)
+  const normalized = result.map(item => normalizeContractOutput(item))
+  return normalized
+  // return (await Moralis.executeFunction(options)).map(item => normalizeContractOutput(item))
+}
