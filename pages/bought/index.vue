d428ef1452fe3031b6941947a0df79cf1cd04819
HabacucMX <habacuc.vera@gmail.com>
2023-03-09 13:17:14 -0600

    bought template

--- pages/bought/index.vue
+++ pages/bought/index.vue
@@ -3,10 +3,22 @@
     <div class="MainScreen">
       <div class="MainBoard">
         <div class="eBoard1">
-          <div class="grid md:grid-cols-4 xl:grid-cols-5 grid-cols-2 px-10 gap-5">
-            <div>
-              <div class="DecoBG">
-                ssdsd
+          <div class="grid md:grid-cols-3 grid-cols-2 px-10 gap-5">
+            <div class="DecoBG grid grid-cols-2 hover:bg-glass-200 cursor-default">
+              <img class="object-scale-down h-64 rounded-tf bg-white/20" src="../../static/product.png">
+              <div class="grid items-center relative">
+                <div>
+                  <div>Product Name</div>
+                  <div class="text-sm">
+                    Sold By Colas
+                  </div>
+                </div>
+                <div class="absolute right-0 top-0 px-3 py-1 bg-deco-100/60 rounded-tr-tf rounded-bl-tf text-xl">
+                  1 DAI
+                </div>
+                <div class="mintButton">
+                  Reedem
+                </div>
               </div>
             </div>
           </div>
