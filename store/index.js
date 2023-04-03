7eb8f60db104e8b470eb233ca8344ee665623380
Krisztian Barta <bartariki@gmail.com>
2023-04-03 11:34:25 +0200

    3dAssets change - Riki

--- store/index.js
+++ store/index.js
@@ -9,20 +9,20 @@ export const state = {
   myAllBoughtServices: [],
 
   businessTypes: [
-    { key: 'store', value: 'products-1', text: 'Convenience Store', imageUrl: 'https://bafybeidk6giaif72qno3idbpzuku47btmwyd2cgkl6owmwkoq4s64wjnla.ipfs.nftstorage.link' },
+    { key: 'store', value: 'products-1', text: 'Convenience Store', imageUrl: 'https://ipfs.moralis.io:2053/ipfs/QmZbE92xWPaqL55s7xWdap1Y3yT2L6eLZP8EjcXczrHANT' },
 
-    { key: 'clothes', value: 'products-2', text: 'Clothing Shop ', imageUrl: 'https://bafybeihx2xihqljwfpsnyvxniyxdns2jk2vi2t2win3ugu5agxfmhdv5vm.ipfs.nftstorage.link' },
+    { key: 'clothes', value: 'products-2', text: 'Clothing Shop ', imageUrl: 'https://ipfs.moralis.io:2053/ipfs/QmeWQiDnFwaSTXR6oHzGnYB5HQkxmQquAkDcfh5WsBULni' },
     //  { key: 'food', value: 'products-2', text: ' Food / Restaurant', imageUrl: 'https://ipfs.moralis.io:2053/ipfs/QmfEbx2aFHNmTZqgye5FNqE3yNLS8vZJm1nTaNHxLfZLn4 ' },
 
-    { key: 'urban', value: 'products-3', text: 'Urban Store', imageUrl: 'https://bafybeiaxoauncqu7t5zdiv6lf3ais7vrwgv3orp4zq74eeasprrvsyy5bm.ipfs.nftstorage.link' },
+    { key: 'urban', value: 'products-3', text: 'Urban Store', imageUrl: 'https://ipfs.moralis.io:2053/ipfs/QmXsPvTTooo1VJqH1D18x7LEfhVoTHCz7eaKYcVqei5yte' },
 
     //  { key: 'fitness', value: 'services-1', text: ' Fitness', imageUrl: 'https://ipfs.moralis.io:2053/ipfs/QmfXw61b7sjPquphjLVivdAas9a8XZT1Bs6BqoAiExzo4r ' },
 
-    { key: 'artwork', value: 'products-4', text: 'Artworks', imageUrl: 'https://bafybeichd5f7x3bmsxoj6nlrfpihep2zis4nmei5mkheln5x5uvgwi6x24.ipfs.nftstorage.link' },
+    { key: 'artwork', value: 'products-4', text: 'Artworks', imageUrl: 'https://ipfs.moralis.io:2053/ipfs/QmZxhaqU17xvr7wRJYcdP897JTPi7BAvZhRyqAVgnFhb41' },
 
-    { key: 'beauty', value: 'products-5', text: 'Beauty Shop', imageUrl: 'https://bafybeihfpwxmxten7loypo43efi5hfblb5m3n3r34al5copluq6lba5e6y.ipfs.nftstorage.link' },
+    { key: 'beauty', value: 'products-5', text: 'Beauty Shop', imageUrl: 'https://ipfs.moralis.io:2053/ipfs/QmRj4eYixtwLX6Bfb5LMzzrZxqQABP6kN75MGpsZ9ZtS7H' },
 
-    { key: 'professional', value: 'products-6', text: 'Profesional Service', imageUrl: 'https://bafybeigxcc4klso32bc4f5nfhpjlsajdueozx2teon6p5qv2wl7l2bgwmu.ipfs.nftstorage.link' }
+    { key: 'professional', value: 'products-6', text: 'Profesional Service', imageUrl: 'https://ipfs.moralis.io:2053/ipfs/QmWdPdsQRWJdEWcBwDb4tWFHLznpznxXU2a131v92znqDq' }
   ]
 }
 
